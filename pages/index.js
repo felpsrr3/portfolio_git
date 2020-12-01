51852c3b80836208074cc86313739259410e3a54
Felipe Ricobello <64718731+felpsrr3@users.noreply.github.com>
2020-12-01 00:34:29 -0300

    firstpage

---
+++ pages/index.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+function Home(){
+    return <div>
+        <h1>HOME</h1>
+    </div>      
+}
+
+function Contador(){
+    /*const [contador, setContador] = useState[1];
+
+    function adicionarContador(){
+        setContador(contador++);
+    }
+
+    return <div>
+            <div>{contador}</div> 
+            <button onClick={adicionarContador}>Adicionar</button> 
+           </div>
+    */
+}
+
+export default Home;
